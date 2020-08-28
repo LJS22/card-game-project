4024d60167ca9836e9424242f31824152019211a
LJS22 <LJS22@users.noreply.github.com>
2020-08-28 15:09:58 +0100

    Updates

---
+++ precache-manifest.5f91a53bb117cc07b16895ec794efc83.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8feb7dfae6d10ebb32436c54922ff13f",
+    "url": "/card-game-project/index.html"
+  },
+  {
+    "revision": "4416702610f770adc664",
+    "url": "/card-game-project/static/css/2.257f4af1.chunk.css"
+  },
+  {
+    "revision": "14899c2cc08faac5a027",
+    "url": "/card-game-project/static/css/main.37e3806d.chunk.css"
+  },
+  {
+    "revision": "4416702610f770adc664",
+    "url": "/card-game-project/static/js/2.363dfe0e.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/card-game-project/static/js/2.363dfe0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14899c2cc08faac5a027",
+    "url": "/card-game-project/static/js/main.20d5c702.chunk.js"
+  },
+  {
+    "revision": "3f67c79570db623c42b8",
+    "url": "/card-game-project/static/js/runtime-main.553cc920.js"
+  },
+  {
+    "revision": "6760cdce0c594ad4e44c1f48a27f7168",
+    "url": "/card-game-project/static/media/DK.6760cdce.jpg"
+  },
+  {
+    "revision": "b296e1635e0f9cbc5ca10643dab29466",
+    "url": "/card-game-project/static/media/bowser.b296e163.png"
+  },
+  {
+    "revision": "074f465e3dd8dd511d3c205abdd5ee96",
+    "url": "/card-game-project/static/media/losingGif.074f465e.gif"
+  },
+  {
+    "revision": "a782540d55f95dfc85a315c587f5d38d",
+    "url": "/card-game-project/static/media/luigi.a782540d.jpg"
+  },
+  {
+    "revision": "e8bbb719dff7201aa84de5495a9c8908",
+    "url": "/card-game-project/static/media/mario.e8bbb719.png"
+  },
+  {
+    "revision": "2d74ed270a91e985f7eb20148cde2d2b",
+    "url": "/card-game-project/static/media/peach.2d74ed27.jpg"
+  },
+  {
+    "revision": "99e77606aa412499eff44794158940b0",
+    "url": "/card-game-project/static/media/toad.99e77606.jpg"
+  },
+  {
+    "revision": "3040aa0896b26be4abe15ea32aaf0cf2",
+    "url": "/card-game-project/static/media/wario.3040aa08.jpg"
+  },
+  {
+    "revision": "6cd29b98404c9ecdf4edb0a4624231aa",
+    "url": "/card-game-project/static/media/yoshi.6cd29b98.jpg"
+  }
+]);
